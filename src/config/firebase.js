2757aeb4a8cc65c5ea60871cb5fbc254c0e88339
mashnoon33 <mashnoon33@gmail.com>
2019-07-08 17:46:38 -0400

    adde firebase api keys back to the code

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,31 +1,31 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  projectId: 'garcker-mash-studio',
-  appId: process.env.REACT_APP_appId,
-}
+	apiKey: "AIzaSyAgA9N_ZLKBjAXTpECCRMafKIafzzuDnZk",
+	authDomain: "garcker-mash-studio.firebaseapp.com",
+	databaseURL: "https://garcker-mash-studio.firebaseio.com",
+	storageBucket: "garcker-mash-studio.appspot.com",
+	appId: "1:551481167824:web:b1f389e80c18dc5a",
+	projectId: "garcker-mash-studio",
+};
 
-firebase.initializeApp(config)
+firebase.initializeApp(config);
 firebase
-  .firestore()
-  .enablePersistence()
-  .catch(function(err) {
-    if (err.code === 'failed-precondition') {
-      // Multiple tabs open, persistence can only be enabled
-      // in one tab at a a time.
-      // ...
-    } else if (err.code === 'unimplemented') {
-      // The current browser does not support all of the
-      // features required to enable persistence
-      // ...
-    }
-  })
-firebase.firestore().settings({ timestampsInSnapshots: true })
+	.firestore()
+	.enablePersistence()
+	.catch(function(err) {
+		if (err.code === "failed-precondition") {
+			// Multiple tabs open, persistence can only be enabled
+			// in one tab at a a time.
+			// ...
+		} else if (err.code === "unimplemented") {
+			// The current browser does not support all of the
+			// features required to enable persistence
+			// ...
+		}
+	});
+firebase.firestore().settings({ timestampsInSnapshots: true });
 
-export default firebase
+export default firebase;
